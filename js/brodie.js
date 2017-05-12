718253b0203932aa4c4ba30676352e0917fb4773
Adam <adamdoman@gmail.com>
2017-05-11 21:45:07 -0500

    added brodie logo change and title change on keyup of search box

--- js/brodie.js
+++ js/brodie.js
@@ -1,7 +1,16 @@
-function brodieFunction() {
-    alert("Hi this is brodies cool page");
+function brodifyLogo() {
+    if($("#lst-ib").val().toLowerCase().includes("brodie")){
+      $("#hplogo").attr('src', 'images/brodielogo_color.png');
+      document.title = 'Brodie';
+    } else {
+      $("#hplogo").attr('src', './Google_files/googlelogo_color_272x92dp.png');
+      document.title = 'Google';
+    }
+
 }
 
 $(document).ready(function() {
-  brodieFunction();
+  $("#lst-ib").on("keyup", function(){
+      brodifyLogo();
+  });
 });
